9d89057355d8f9bd1ef352c44b0b80a24a80c3c6
leon <freeyecheng@gmail.com>
2021-05-04 21:33:06 +0800

    java beans

---
+++ java-beans/src/main/java/com/hyperleon/thinking/spring/java/beans/BeanInfoDemo.java
@@ -0,0 +1,35 @@
+package com.hyperleon.thinking.spring.java.beans;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.beans.PropertyEditorSupport;
+import java.util.stream.Stream;
+
+/**
+ * @author leon
+ * @date 2021-05-04 21:24
+ **/
+public class BeanInfoDemo {
+
+    public static void main(String[] args) throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+
+        Stream.of(propertyDescriptors).forEach((descriptor) -> {
+            System.out.println(descriptor);
+            Class<?> propertyType = descriptor.getPropertyType();
+            if ("age".equals(descriptor.getName())) {
+                descriptor.setPropertyEditorClass(StringToIntegerPropertyEditor.class);
+            }
+        });
+    }
+
+    static class StringToIntegerPropertyEditor extends PropertyEditorSupport {
+        @Override
+        public void setAsText(String text) throws java.lang.IllegalArgumentException {
+            setValue(Integer.valueOf(text));
+        }
+    }
+}
---
+++ java-beans/src/main/java/com/hyperleon/thinking/spring/java/beans/Person.java
@@ -0,0 +1,28 @@
+package com.hyperleon.thinking.spring.java.beans;
+
+/**
+ * @author leon
+ * @date 2021-05-04 21:23
+ **/
+public class Person {
+
+    String name;
+
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
